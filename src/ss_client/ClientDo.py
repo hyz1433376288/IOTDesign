334d060a48f755ddfa045c5b18b7b53502eee4d2
hyz1433376288 <64128854+hyz1433376288@users.noreply.github.com>
2021-04-22 13:53:01 +0800

    init

--- src/ss_client/ClientDo.py
+++ src/ss_client/ClientDo.py
@@ -1,10 +1,7 @@
 import include.Decode
 import include.Encode
-import os
-import shutil
 
 def set_msg(mac, host, data):
-
     return mac + host + data
 
 def set_device(msg):
@@ -15,5 +12,3 @@ def update(head_temp_humi, tail_setted_device, temp_humi_len):
     # print("@{}\n@{}".format(head_temp_humi[:temp_humi_len],tail_setted_device[temp_humi_len:]))
     return  head_temp_humi[:temp_humi_len] + tail_setted_device[temp_humi_len:]
 # print(update("2046051179282426554210202506200","2013087166292426554210202506211",10))
-def makefile(addr, temperature, humidity, data):
-    
