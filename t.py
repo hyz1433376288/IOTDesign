878083410b9f751e83c9d411ef76826779731c06
hyz1433376288 <64128854+hyz1433376288@users.noreply.github.com>
2021-04-22 16:42:57 +0800

    move /greenhouse/ to /web/ ,and rename it as smarthome/

--- t.py
+++ t.py
@@ -1,6 +1,14 @@
 
+import json
 
-s = "0 1 2 44 55 66"
-a = s.split(" ")
-
-print(a)
+a = [i for i in range(0, 5)]
+data = {
+    'name':"sam",
+    'length':a
+}
+filename = "data.json"
+with open(filename, 'w') as file_obj:
+    json.dump(data, file_obj)
+with open(filename, 'r') as file_obj:
+    jstr = json.load(file_obj)
+    print(jstr)
